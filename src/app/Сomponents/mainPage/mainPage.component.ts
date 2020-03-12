7689a39f14c1e580e502c1e2739fb5a548fd2373
K-Krait <k.krait8@gmail.com>
2020-03-12 13:57:32 +0300

    доработал функционал добавления карт

--- src/app/Сomponents/mainPage/mainPage.component.ts
+++ src/app/Сomponents/mainPage/mainPage.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { Card } from "../../Entities/card";
 import { logoRepository } from "../../Entities/logoRepository";
-import { inputFileReaderComponent } from "../inputFileReader/InputFileReader.component";
-
 
 @Component({
   selector: "app-main",
   templateUrl: "./mainPage.component.html",
   styleUrls: ["./mainPage.component.css"]
 })
-export class mainPageComponent implements OnInit {
+export class mainPageComponent {
   addBookFormVisibility: boolean = false;
   addBookBtnVisibility: boolean = true;
   cardDeckVisibility: boolean = true;
 
   logo = new logoRepository();
-  
 
   card: Card = new Card();
   cards: Card[] = [
@@ -33,19 +30,16 @@ export class mainPageComponent implements OnInit {
     }
   ];
 
-  filereader = new inputFileReaderComponent();
-
   constructor() {}
 
   getCards() {
     return this.cards;
   }
-
+  
   addBook() {
     this.addBookBtnVisibility = false;
     this.cardDeckVisibility = false;
     this.addBookFormVisibility = true;
-    
   }
 
   createBook() {
@@ -62,5 +56,21 @@ export class mainPageComponent implements OnInit {
     this.cardDeckVisibility = true;
   }
 
-  ngOnInit() {}
+  imgUrlBook: any;
+
+  changeListener($event: any): void {
+    this.readThis($event.target);
+  }
+
+  readThis(inputValue: any): void {
+    var file: File = inputValue.files[0];
+    var reader: FileReader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      // return reader.result;
+      console.log(reader.result);
+      this.imgUrlBook = reader.result;
+    };
+  }
 }
